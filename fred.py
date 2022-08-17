d9bb48329223f17c743cf4eb9ca7f0756bdf0b4e
archisdhar <dhar8@purdue.edu>
2022-08-16 18:19:15 -0700

    added more FRED categories

--- fred.py
+++ fred.py
@@ -8,15 +8,15 @@ path = os.getcwd()
 fred = Fred(api_key='13f1e0b5cbdcb8307bbf7bbca9852e4a')
 
 def main():
-    getData(['GDP', 'UNrate'])
+    getData({'GDP': 'GDP', 'UNrate': 'Unemployment Rate', 'GDPC1': 'Real GDP'})
 
 def getData(values):
     for x in values:
         data = fred.get_series_latest_release(x)
         data = data.reset_index()
-        data.columns = ['Date',  x]
+        data.columns = ['Date',  values[x]]
         data.to_csv(path + '/macroecondata/' + x + '.csv')
-#categories = ['UNrate', 'GDP', 'SP500','CPALTT01USM657N' , FPCPITOTLZGUSA (inflation), GDPC1 (real gdp), IMPGSC1 (real imports of goods), NETEXC (real exports), DGDSRX1Q020SBEA (personal consumption of goods), CORESTICKM159SFRBATL (CPI), DFF (Fed rate hikes), UMCSENT (consumer sentiment)]
+#categories = ['UNrate', 'GDP', 'SP500','CPALTT01USM657N' , FPCPITOTLZGUSA (inflation), GDPC1 (real gdp), IMPGSC1 (real imports of goods), NETEXC (real exports), DGDSRX1Q020SBEA (personal consumption of goods), CORESTICKM159SFRBATL (CPI), DFF (Fed rate hikes), UMCSENT (consumer sentiment), RSXFS (retail sales), PPIACO (Producer price index), RECPROUSM156N (smoothed US rececssion probability)]
 
 if __name__ == "__main__":
     main()
